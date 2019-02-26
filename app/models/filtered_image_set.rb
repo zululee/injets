8ed382adcd4ee450b2fda9c9442700d51310c7de
lz <zakkalz@amazon.com>
2019-02-26 13:04:32 -0500

    adding controller and model

---
+++ app/models/filtered_image_set.rb
@@ -0,0 +1,31 @@
+class FilteredImageSet < Set
+  attr_reader :subject_filter
+
+  def initialize(collection=[], subject_filter: 'hot dog')
+    @subject_filter = subject_filter
+    @rekognition = Aws::Rekognition::Client.new
+    super(collection)
+  end
+
+  def << (image)
+    begin
+      super if subject_detected_in?(image)
+    rescue
+      #notify error tracker
+    end
+    image
+  end
+
+  private
+
+  def subject_detected_in?(image)
+    response = @rekognition.detect_labels({
+      image: {
+        bytes: image.bytes
+      }
+    })
+    labels = response.labels.map{ |label| label.name.downcase }
+    matches = [ @subject_filter, @subject_filter.singularize ] & labels
+    matches.present?
+  end
+end
